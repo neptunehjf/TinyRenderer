1456c27c08a0b33aa368c68c9b7c2299fa553d32
neptunehjf <neptunehjf@sina.com>
2025-03-09 15:56:05 +0900

    Lesson 1 Bresenham’s Line Drawing Algorithm bugfix and model
    1 修复画线bug，改为先 消除斜率的影响 再消除参数输入顺序的影响
    2 画模型

--- Lesson 1 Bresenham’s Line Drawing Algorithm/main.cpp
+++ Lesson 1 Bresenham’s Line Drawing Algorithm/main.cpp
@@ -1,7 +1,12 @@
 #include "tgaimage.h"
+#include "geometry.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
 
 using namespace std;
 
@@ -121,23 +126,23 @@ using namespace std;
 // 参考Referrence/Bresenham Algorithm.jpg
 void line(int x0, int y0, int x1, int y1, TGAImage& image,const TGAColor& color)
 {
-	// 1 消除参数输入顺序的影响
+
+	// 1 消除斜率的影响
+	// 因为采样频率是由x方向的变化频率决定的，y当方向变化频率大于x方向的变化频率的时候，采样频率可能跟不上y轴的变化频率，导致失真
+	bool steep = false;
+	if (abs(x1 - x0) < abs(y1 - y0))
+	{
+		steep = true;
+		// 转置，始终在高频的方向上采样
+		swap(x0, y0);
+		swap(x1, y1);
+	}
+
+	// 2 消除参数输入顺序的影响
 	if (x0 > x1)
 	{
 		swap(x0, x1);
 		swap(y0, y1);
-	}
-	// 2 消除斜率的影响
-	// 因为采样频率是由x方向的变化频率决定的，y当方向变化频率大于x方向的变化频率的时候，采样频率可能跟不上y轴的变化频率，导致失真
-	bool steep = false;
-	if (abs(x1 - x0) < abs(y1 - y0))
-		steep = true;
-
-	if (steep)
-	{
-		// 转置，始终在高频的方向上采样
-		swap(x0, y0);
-		swap(x1, y1);
 	}
 
 	// 如有转置，要在转置之后再计算增量
@@ -166,19 +171,69 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image,const TGAColor& color)
 	}
 }
 
+//void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+//	bool steep = false;
+//	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+//		std::swap(x0, y0);
+//		std::swap(x1, y1);
+//		steep = true;
+//	}
+//	if (x0 > x1) {
+//		std::swap(x0, x1);
+//		std::swap(y0, y1);
+//	}
+//	int dx = x1 - x0;
+//	int dy = y1 - y0;
+//	int derror2 = std::abs(dy) * 2;
+//	int error2 = 0;
+//	int y = y0;
+//	for (int x = x0; x <= x1; x++) {
+//		if (steep) {
+//			image.set(y, x, color);
+//		}
+//		else {
+//			image.set(x, y, color);
+//		}
+//		error2 += derror2;
+//		if (error2 > dx) {
+//			y += (y1 > y0 ? 1 : -1);
+//			error2 -= dx * 2;
+//		}
+//	}
+//}
+
 int main(int argc, char** argv) 
 {
-	TGAImage image(100, 100, TGAImage::RGB);
-
-	for (int i = 0; i < 1000000; i++) // for 性能分析
+	if (2 == argc) 
 	{
-		line(13, 20, 80, 40, image, white);
-		line(20, 13, 40, 80, image, red);
-		line(80, 40, 13, 20, image, red);
+		model = new Model(argv[1]);
+	}
+	else 
+	{
+		model = new Model("obj/african_head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
+
+	for (int i = 0; i < model->nfaces(); i++) 
+	{
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) 
+		{
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
 	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
+	delete model;
+
 	return 0;
 }
 
