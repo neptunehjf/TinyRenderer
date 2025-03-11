0289d9a0f6c7a4505097ba3dbe99fe3145c14b17
neptunehjf <neptunehjf@sina.com>
2025-03-11 13:41:39 +0900

    triangle_line_scan
    // 线扫描法画填充三角形
    // 1 把三角形分为左右两条边A,B，注意B分为上下两部分
    // 2 根据y方向的变化率线性计算出A.x B.x
    // 3 根据从A.x B.x逐行从左向右画线

--- Lesson 2 Triangle rasterization and back face culling/main.cpp
+++ Lesson 2 Triangle rasterization and back face culling/main.cpp
@@ -4,9 +4,10 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = NULL;
-const int width = 800;
-const int height = 800;
+const int width = 200;
+const int height = 200;
 
 using namespace std;
 
@@ -61,33 +62,73 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image,const TGAColor& color)
 	}
 }
 
+// 线扫描法画填充三角形
+// 1 把三角形分为左右两条边A,B，注意B分为上下两部分
+// 2 根据y方向的变化率线性计算出A.x B.x
+// 3 根据从A.x B.x逐行从左向右画线
+void triangle_line_scan(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+{
+	// 退化三角形不处理
+	if (t0.x == t1.x && t0.x == t2.x || t0.y == t1.y && t0.y == t2.y) return;
+
+	// 冒泡排序，使t0 t1 t2按y轴从小到大的顺序排序
+	if (t0.y > t1.y) swap(t0, t1);
+	if (t0.y > t2.y) swap(t0, t2);
+	if (t1.y > t2.y) swap(t1, t2);
+
+	// 总扫描行数
+	int total_line = t2.y - t0.y;
+
+	// 逐行扫描
+	for (int i = 0; i <= total_line; i++)
+	{
+		bool upper_half = i > t1.y - t0.y || t0.y == t1.y;	// 是否是B边界的上半部分
+		int segment_line = upper_half ? (t2.y - t1.y) : (t1.y - t0.y);
+		float alpha = (float)i / total_line;
+		float beta = (float)(i - (upper_half ? (t1.y - t0.y) : 0)) / segment_line;
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = upper_half ? (t1 + (t2 - t1) * beta) : (t0 + (t1 - t0) * beta);
+		if (A.x > B.x) swap(A, B); // 边界A在左，B在右
+		for (int j = A.x; j <= B.x; j++)
+			image.set(j, t0.y + i, color);
+	}
+}
+
 int main(int argc, char** argv) 
 {
-	if (2 == argc) 
-	{
-		model = new Model(argv[1]);
-	}
-	else 
-	{
-		model = new Model("obj/african_head.obj");
-	}
+	//if (2 == argc) 
+	//{
+	//	model = new Model(argv[1]);
+	//}
+	//else 
+	//{
+	//	model = new Model("obj/african_head.obj");
+	//}
 
 	TGAImage image(width, height, TGAImage::RGB);
 
-	for (int i = 0; i < model->nfaces(); i++) 
-	{
-		std::vector<int> face = model->face(i);
-		for (int j = 0; j < 3; j++) 
-		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			line(x0, y0, x1, y1, image, white);
-		}
-	}
+	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+
+	triangle_line_scan(t0[0], t0[1], t0[2], image, red);
+	triangle_line_scan(t1[0], t1[1], t1[2], image, white);
+	triangle_line_scan(t2[0], t2[1], t2[2], image, green);
+
+	//for (int i = 0; i < model->nfaces(); i++) 
+	//{
+	//	std::vector<int> face = model->face(i);
+	//	for (int j = 0; j < 3; j++) 
+	//	{
+	//		Vec3f v0 = model->vert(face[j]);
+	//		Vec3f v1 = model->vert(face[(j + 1) % 3]);
+	//		int x0 = (v0.x + 1.) * width / 2.;
+	//		int y0 = (v0.y + 1.) * height / 2.;
+	//		int x1 = (v1.x + 1.) * width / 2.;
+	//		int y1 = (v1.y + 1.) * height / 2.;
+	//		line(x0, y0, x1, y1, image, white);
+	//	}
+	//}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
