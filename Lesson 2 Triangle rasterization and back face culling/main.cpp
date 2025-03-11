0d61aaaf2e5fcdef83267e50224f83df332044b9
neptunehjf <neptunehjf@sina.com>
2025-03-11 12:17:31 +0900

    Lesson 2 Triangle rasterization and back face culling new project
    new project

---
+++ Lesson 2 Triangle rasterization and back face culling/main.cpp
@@ -0,0 +1,98 @@
+#include "tgaimage.h"
+#include "geometry.h"
+#include "model.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
+
+using namespace std;
+
+// Bresenham算法画直线
+// 把除法从循环里拿出来了，并且消除了float计算。理论上速度更快
+// 虽然有些微优化，并没有教程中那么明显，可能是编译器不一样(MSVC vs GCC)？
+// 参考Referrence/Bresenham Algorithm.jpg
+void line(int x0, int y0, int x1, int y1, TGAImage& image,const TGAColor& color)
+{
+
+	// 1 消除斜率的影响
+	// 因为采样频率是由x方向的变化频率决定的，y当方向变化频率大于x方向的变化频率的时候，采样频率可能跟不上y轴的变化频率，导致失真
+	bool steep = false;
+	if (abs(x1 - x0) < abs(y1 - y0))
+	{
+		steep = true;
+		// 转置，始终在高频的方向上采样
+		swap(x0, y0);
+		swap(x1, y1);
+	}
+
+	// 2 消除参数输入顺序的影响
+	if (x0 > x1)
+	{
+		swap(x0, x1);
+		swap(y0, y1);
+	}
+
+	// 如有转置，要在转置之后再计算增量
+	int dx = abs(x1 - x0);
+	int dy = abs(y1 - y0);
+
+	int de = 2 * dy;
+	int e = 0.0;
+	int y = y0;
+
+	for (int x = x0; x <= x1; x++)
+	{
+		if (steep)
+			image.set(y, x, color); // 转置只是为了采样，输出颜色的时候还得转置回去
+		else
+			image.set(x, y, color);
+
+		e += de;
+		
+		if (e > dx)
+		{
+			y += (y0 < y1 ? 1 : -1); // 根据y的沿伸方向加1或减1
+			e -= 2 * dx; //因为y的增量有可能不到1，这里计算补正量。比如y增量是0.6>0.5，于是y加1，
+			//实际多增加了1 - 0.6 = 0.4，所以补正量是-0.4，要加到下一次的增量计算里
+		}
+	}
+}
+
+int main(int argc, char** argv) 
+{
+	if (2 == argc) 
+	{
+		model = new Model(argv[1]);
+	}
+	else 
+	{
+		model = new Model("obj/african_head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
+
+	for (int i = 0; i < model->nfaces(); i++) 
+	{
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) 
+		{
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
+
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+	delete model;
+
+	return 0;
+}
+
